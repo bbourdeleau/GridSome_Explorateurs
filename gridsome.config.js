d343c4b92cf8f99157258c2d40f1b0ed29d2a0aa
bbourdeleau <44268607+bbourdeleau@users.noreply.github.com>
2019-08-02 16:57:36 +0200

    Add files via upload

---
+++ gridsome.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteName: 'explorateurs-du-frontend',
+  siteDescription: 'découvrir le frontend',
+  plugins: [
+    {
+      options: {
+        baseUrl: 'http://explorateurs-du-frontend/', // required
+        typeName: 'WordPress', // GraphQL schema name (Optional)
+        perPage: 100, // How many posts to load from server per request (Optional)
+        concurrent: 10, // How many requests to run simultaneously (Optional)
+        routes: {
+          post: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
+          post_tag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+        }
+      }
+    }
+  ]
+}
